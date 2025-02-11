6b2025b332a29e88c0a2674df76b3449e23b4fb6
TheMaZe <shitov.1497@bk.ru>
2025-02-11 17:47:31 +0300

    Hiden secret key

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-++jsi&(vxk@^$i9-t4d4gt*%7x1&@smtct2q+!=sl-u4d=6snl'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
